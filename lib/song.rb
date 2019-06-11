f59f16061ccd17c0fb72b411b4b99663cc6122dc
Mark May <mamark90@gmail.com>
2019-06-11 00:59:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,67 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+      self.find_by_name(name) ||
+      self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort {|a, b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(filename)
+    r1 = /\s[-.]\s/
+    r2 = /[.]/
+    file_info = filename.split(Regexp.union(r1, r2))
+    artist_name = file_info[0]
+    song_name = file_info[1]
+
+    song = self.new
+    song.artist_name = artist_name
+    song.name = song_name
+    song
+  end
+
+  def create_from_filename(filename)
+
+    r1 = /\s[-.]\s/
+    r2 = /[.]/
+    file_info = filename.split(Regexp.union(r1, r2))
+    artist_name = file_info[0]
+    song_name = file_info[1]
+
+    song = self.new
+    artist_name = file_info[0]
+    song.name = file_info[1]
+    @@all << song
+    song
+    binding.pry
+  end
+
 
 end
